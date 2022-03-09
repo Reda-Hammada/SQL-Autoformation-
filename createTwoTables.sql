d972530aa3960599fd55a395eddf366e357b0ac9
reda hammada <hammada.reda.dev@gmail.com>
2022-03-09 18:30:49 +0100

    update

---
+++ createTwoTables.sql
@@ -0,0 +1,15 @@
+USE `student`;
+
+CREATE TABLE students(
+idf int PRIMARY KEY,
+name varchar (250)
+
+
+);
+
+CREATE TABLE notes (
+
+    idf int (250),
+    id int PRIMARY KEY,
+    note float (250) 
+);
